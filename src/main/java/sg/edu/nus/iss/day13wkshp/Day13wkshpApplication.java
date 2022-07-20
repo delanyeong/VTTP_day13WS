b11150905f5304a040e5c2641d490f6d2b9990b1
delanyeong <delan.april@gmail.com>
2022-07-20 09:58:56 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/day13wkshp/Day13wkshpApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day13wkshp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day13wkshpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day13wkshpApplication.class, args);
+	}
+
+}
